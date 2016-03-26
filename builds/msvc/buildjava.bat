c56b7ab5622fc1581e08e93d840d0091d1fd576b
Kenneth Long <klong15@mail.greenriver.edu>
2016-03-25 21:23:22 -0700

    javah uses classpath, removed change dirs

--- builds/msvc/buildjava.bat
+++ builds/msvc/buildjava.bat
@@ -6,17 +6,12 @@ echo build java classes, java should be in path
 javac ..\..\jzmq-jni\src\main\java\org\zeromq\EmbeddedLibraryTools.java ..\..\jzmq-jni\src\main\java\org\zeromq\ZMQException.java ..\..\jzmq-jni\src\main\java\org\zeromq\ZMQ.java ..\..\jzmq-jni\src\main\java\org\zeromq\App.java ..\..\jzmq-devices\src\main\java\org\zeromq\ZMQForwarder.java ..\..\jzmq-devices\src\main\java\org\zeromq\ZMQQueue.java ..\..\jzmq-core\src\main\java\org\zeromq\Utils.java ..\..\jzmq-devices\src\main\java\org\zeromq\ZMQStreamer.java ..\..\jzmq-core\src\main\java\org\zeromq\ZContext.java ..\..\jzmq-core\src\main\java\org\zeromq\ZAuth.java ..\..\jzmq-core\src\main\java\org\zeromq\ZDispatcher.java ..\..\jzmq-core\src\main\java\org\zeromq\ZFrame.java ..\..\jzmq-core\src\main\java\org\zeromq\ZMsg.java ..\..\jzmq-core\src\main\java\org\zeromq\ZThread.java ..\..\jzmq-core\src\main\java\org\zeromq\ZLoop.java
 
 echo create jni headers for jzmq-jni
-cd ..\..\jzmq-jni\src\main\java
-javah -jni org.zeromq.ZMQException org.zeromq.EmbeddedLibraryTools org.zeromq.ZMQ org.zeromq.App
-
+javah -jni -sourcepath ..\..\jzmq-jni\src\main\java org.zeromq.ZMQException org.zeromq.EmbeddedLibraryTools org.zeromq.ZMQ org.zeromq.App
 
 echo create jni headers for jzmq-core
-cd ..\..\..\..\jzmq-core\src\main\java
-javah -jni org.zeromq.Utils org.zeromq.ZAuth org.zeromq.ZContext org.zeromq.ZDispatcher org.zeromq.ZFrame org.zeromq.ZLoop org.zeromq.ZMsg org.zeromq.ZThread
+javah -jni -sourcepath ..\..\jzmq-core\src\main\java org.zeromq.Utils org.zeromq.ZAuth org.zeromq.ZContext org.zeromq.ZDispatcher org.zeromq.ZFrame org.zeromq.ZLoop org.zeromq.ZMsg org.zeromq.ZThread
 
 echo create jni headers for jzmq-devices
-cd ..\..\..\..\jzmq-devices\src\main\java
-javah -jni org.zeromq.ZMQForwarder org.zeromq.ZMQQueue org.zeromq.ZMQStreamer
+javah -jni -sourcepath ..\..\jzmq-devices\src\main\java org.zeromq.ZMQForwarder org.zeromq.ZMQQueue org.zeromq.ZMQStreamer
 
-cd ..\..\..\..\builds\msvc
 REM echo on
